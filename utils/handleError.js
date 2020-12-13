fb878aa67e4d3a483cea9ed59f53bef2a9cd211f
Sagar Gavhane <sagar.gavhane04@gmail.com>
2020-12-13 18:24:09 +0530

    📦 NEW(function): handleError

---
+++ utils/handleError.js
@@ -0,0 +1,23 @@
+import httpStatusCodes from 'http-status-codes'
+import { Error } from 'mongoose'
+
+export function handleError(res, err) {
+  if (err instanceof Error.ValidationError) {
+    let message = {}
+
+    Object.keys(err.errors).map((key) => {
+      const properties = err.errors[key].properties
+      message[key] = properties.message
+    })
+
+    res.status(httpStatusCodes.UNPROCESSABLE_ENTITY).json({
+      message,
+      code: 'ValidationError',
+    })
+  }
+
+  res.status(httpStatusCodes.UNPROCESSABLE_ENTITY).json({
+    message: err.message,
+    code: err.code ?? 'UNPROCESSABLE_ENTITY',
+  })
+}
