90ea9b692a9471c7b26ef8b1713a220290919ffb
Sagar Gavhane <sagar.gavhane04@gmail.com>
2020-12-15 23:13:27 +0530

    add: sales component

---
+++ pages/sales/invoice/update/[invoiceId].js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Error from 'next/error'
+import { Spin } from 'antd'
+import { useRouter } from 'next/router'
+import { useQuery } from 'react-query'
+
+import SalesInvoiceForm from 'components/SalesInvoiceForm'
+import salesService from 'services/sales'
+
+export default function SalesInvoiceUpdatePage() {
+  const router = useRouter()
+
+  const { data, isLoading, error } = useQuery(
+    ['/sales/invoice/', { invoiceId: router.query.invoiceId }],
+    (_, { invoiceId }) => salesService.invoice.get(invoiceId),
+    {
+      enabled: typeof router.query.invoiceId !== 'undefined',
+    }
+  )
+
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          display: 'grid',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+        }}
+      >
+        <Spin />
+      </div>
+    )
+  }
+
+  if (error) {
+    return <Error statusCode={500} title={error} />
+  }
+
+  return <SalesInvoiceForm actionType='EDIT_INVOICE' invoice={data} />
+}
