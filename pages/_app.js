4374552089d2e2c9b041caddfb7d9252e72e066e
Sagar Gavhane <sagar.gavhane04@gmail.com>
2020-12-15 23:03:08 +0530

    add _app.js file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import React, { Fragment } from 'react'
+import Head from 'next/head'
+import { QueryCache, ReactQueryCacheProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query-devtools'
+import 'antd/dist/antd.css'
+import 'nprogress/nprogress.css'
+import 'styles/global.css'
+
+import AuthContext, { authInitialState } from 'context/AuthContext'
+import useLocalStorage from 'hooks/useLocalStorage'
+
+const queryCache = new QueryCache({
+  defaultConfig: {
+    queries: {
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useLocalStorage('auth', authInitialState)
+
+  return (
+    <Fragment>
+      <Head>
+        <title>RKChicken</title>
+      </Head>
+      <AuthContext.Provider value={[user, setUser]}>
+        <ReactQueryCacheProvider queryCache={queryCache}>
+          <ReactQueryDevtools />
+          <Component {...pageProps} />
+        </ReactQueryCacheProvider>
+      </AuthContext.Provider>
+    </Fragment>
+  )
 }
 
 export default MyApp
