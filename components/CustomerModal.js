43a74c74a2fa09d04262d4cb5496fc64bc220507
Sagar Gavhane <sagar.gavhane04@gmail.com>
2020-12-15 22:56:59 +0530

    add <CustomerModel />

---
+++ components/CustomerModal.js
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import { Modal, Form, Input, message } from 'antd'
+import { useQueryCache } from 'react-query'
+
+import validationRules from 'config/validationRules'
+import {
+  createCustomerService,
+  updateCustomerService,
+} from 'services/customers'
+
+const CustomerModal = (props) => {
+  const [form] = Form.useForm()
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const cache = useQueryCache()
+
+  const defaultFormValues = {
+    name: '',
+    mobileNumber: '',
+    alternativeMobileNumber: '',
+    discountRate: 10,
+  }
+
+  useEffect(() => {
+    if (props.modalType === 'EDIT_CUSTOMER_MODAL') {
+      form.setFieldsValue(props.customer)
+    } else {
+      form.setFieldsValue(defaultFormValues)
+    }
+  }, [props.customer])
+
+  return (
+    <Modal
+      title={
+        props.modalType === 'EDIT_CUSTOMER_MODAL'
+          ? 'Edit customer'
+          : 'New customer'
+      }
+      centered
+      visible={props.visible}
+      cancelButtonProps={{ disabled: isSubmitting }}
+      onCancel={props.onCancel}
+      okText='Submit'
+      okButtonProps={{ disabled: isSubmitting }}
+      onOk={() => {
+        form
+          .validateFields()
+          .then(async (values) => {
+            setIsSubmitting(true)
+
+            if (props.modalType === 'EDIT_CUSTOMER_MODAL') {
+              await updateCustomerService(props.customer._id, values)
+              message.success('Customer has been updated successfully.')
+            } else {
+              await createCustomerService(values)
+              message.success('Customer has been created successfully.')
+            }
+
+            cache.invalidateQueries(['/customers'])
+            form.resetFields()
+            props.onCancel()
+          })
+          .catch((err) => {
+            message.error(err.message)
+            console.log('Validate Failed:', err)
+          })
+          .finally(() => setIsSubmitting(false))
+      }}
+    >
+      <Form
+        name='customer-form'
+        id='customer-form'
+        layout='vertical'
+        form={form}
+        requiredMark={false}
+      >
+        <Form.Item
+          label='Customer name'
+          name='name'
+          rules={validationRules.customerName}
+        >
+          <Input autoFocus />
+        </Form.Item>
+        <Form.Item
+          label='Mobile number'
+          name='mobileNumber'
+          rules={validationRules.mobileNumber}
+        >
+          <Input type='number' />
+        </Form.Item>
+        <Form.Item
+          label='Alternative mobile number'
+          name='alternativeMobileNumber'
+          rules={validationRules.alternativeMobileNumber}
+        >
+          <Input type='number' />
+        </Form.Item>
+        <Form.Item
+          label='Discount rate'
+          name='discountRate'
+          rules={validationRules.discountRate}
+        >
+          <Input
+            type='number'
+            prefix={<span>₹</span>}
+            defaultValue={10}
+            min={0}
+          />
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
+
+CustomerModal.defaultProps = {
+  modalType: 'EDIT_CUSTOMER_MODAL',
+  visible: false,
+  customer: {},
+  onCancel: () => {},
+}
+
+CustomerModal.propTypes = {
+  modalType: PropTypes.oneOf(['EDIT_CUSTOMER_MODAL', 'CREATE_CUSTOMER_MODAL'])
+    .isRequired,
+  visible: PropTypes.bool.isRequired,
+  customer: PropTypes.object.isRequired,
+  onCancel: PropTypes.func.isRequired,
+}
+
+export default CustomerModal
