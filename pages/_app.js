20b02b56e796e221176b4e7d314db6bdcd40873d
Sagar Gavhane <sgavhane70@gmail.com>
2023-07-20 14:19:07 +0530

    remove: gtag and hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,7 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment } from 'react'
 import Head from 'next/head'
 import { QueryCache, ReactQueryCacheProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query-devtools'
-import { useRouter } from 'next/router'
-import * as gtag from 'utils/gtag'
 import 'antd/dist/antd.css'
 import 'nprogress/nprogress.css'
 import 'styles/global.css'
@@ -20,18 +18,7 @@ const queryCache = new QueryCache({
 })
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
   const [user, setUser] = useLocalStorage('auth', authInitialState)
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
 
   return (
     <Fragment>
--- pages/_document.js
+++ pages/_document.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
 
-import { GA_TRACKING_ID } from 'utils/gtag'
-
 class CustomDocument extends NextDocument {
   render() {
     return (
@@ -10,35 +8,6 @@ class CustomDocument extends NextDocument {
         <Head>
           {this.props.styleTags}
           <meta name='title' content='RKChicken.com' />
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-                });
-              `,
-            }}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `(function(h,o,t,j,a,r){
-                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                  h._hjSettings={hjid:2357143,hjsv:6};
-                  a=o.getElementsByTagName('head')[0];
-                  r=o.createElement('script');r.async=1;
-                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                  a.appendChild(r);
-                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-              `,
-            }}
-          />
         </Head>
         <body>
           <Main />
