3697c4564f11905fbcf9389526e58041f51458cd
Sagar Gavhane <sgavhane70@gmail.com>
2023-08-02 02:01:25 +0530

    fix: revalidate immediately

--- pages/api/customers.js
+++ pages/api/customers.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
     if (cached) {
       res.setHeader(
         'Cache-Control',
-        'public, max-age=60, s-maxage=60, stale-while-revalidate=30'
+        'public, max-age=60, s-maxage=60, stale-while-revalidate'
       )
       res.status(httpStatusCodes.OK).json({
         message: 'Customers record successfully retrieved.',
@@ -35,7 +35,7 @@ export default async function handler(req, res) {
 
         res.setHeader(
           'Cache-Control',
-          'public, max-age=60, s-maxage=60, stale-while-revalidate=30'
+          'public, max-age=60, s-maxage=60, stale-while-revalidate'
         )
         res.status(httpStatusCodes.OK).json({
           message: 'Customers record successfully retrieved.',
