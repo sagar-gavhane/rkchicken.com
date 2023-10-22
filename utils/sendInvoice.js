3bfad5097ba853d9ce9568d356a086b207ff5693
sagar <sgavhane70@gmail.com>
2023-10-22 11:31:06 +0530

    fix: hard coded base url

--- utils/sendInvoice.js
+++ utils/sendInvoice.js
@@ -2,6 +2,8 @@ import format from 'date-fns/format'
 
 import { sendMessage } from 'utils/sendMessage'
 
+const baseUrl = process.env.NEXT_PUBLIC_URL
+
 export function sendInvoice(customer, invoice) {
   const mobile = [customer.mobileNumber]
 
@@ -9,7 +11,7 @@ export function sendInvoice(customer, invoice) {
     `Rk Chicken Center`,
     `Shivajivadi, Moshi`,
     `Date: ${format(new Date(invoice.invoiceDate), 'dd/MM/yyyy')}`,
-    `Bill details: https://rkchichen.com/s/${invoice.shortKey}`,
+    `Bill details: ${baseUrl}/s/${invoice.shortKey}`,
   ].join('\n')
 
   if (customer.alternativeMobileNumber) {
